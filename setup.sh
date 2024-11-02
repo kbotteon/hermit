1b489609d313f1d7c5fdc72cec936bd1deb3e116
Kyle Botteon <49845890+kbotteon@users.noreply.github.com>
2024-11-02 14:36:41 -0700

    chore: clean up setup.sh checks and --clean arg

--- setup.sh
+++ setup.sh
@@ -1,24 +1,69 @@
 #!/usr/bin/env bash
+################################################################################
+# \brief Environment setup scripts
+#
+# (C) 2024 Kyle Botteon
+# This file is part of HERMIT. Refer to the LICENSE in that repository.
+################################################################################
 
 VENV_DIR="./.venv"
 START_DIR=$(pwd)
+SCRIPT_PATH="${BASH_SOURCE[0]}"
 
-if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-    printf "This script should be sourced, not executed\n"
-    exit 1
+################################################################################
+# Check Preconditions
+################################################################################
+
+# Always execute out of script directory
+cd $(dirname "$(readlink -f "${SCRIPT_PATH}")"})
+
+# Script must be sourced to work
+if [[ "${SCRIPT_PATH}" == "${0}" ]]; then
+    echo "ERROR: This script should be sourced, not executed"
+    return 1
 fi
 
-cd $(dirname "$(readlink -f "${BASH_SOURCE[0]}")"})
+# Ensure python3 is installed
+if ! command -v python3 &>/dev/null; then
+    echo "ERROR: python3 is not installed"
+    return 1
+fi
 
-# TODO: Ensure python3 is installed
-# TODO: Ensure python3-venv is installed
+# Ensure python3-venv is installed
+if ! python3 -c "import venv" &>/dev/null; then
+    echo "ERROR: python3-venv is not installed"
+    return 1
+fi
+
+################################################################################
+# Process Arguments
+################################################################################
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -c|--clean)
+            deactivate
+            rm -rf "${VENV_DIR}"
+            shift
+            ;;
+        *)
+            echo "Invalid option ${1}" >&2
+            return 1
+            ;;
+    esac
+done
+
+################################################################################
+# Run Script
+################################################################################
+
 
 if [ ! -d "${VENV_DIR}" ]; then
-    python3 -m venv ${VENV_DIR}
-    source ${VENV_DIR}/bin/activate
+    python3 -m venv "${VENV_DIR}"
+    source "${VENV_DIR}/bin/activate"
     pip install --upgrade pip
     pip install setuptools wheel
-    pip install -r ./script/requirements.txt
+    pip install -r ./tools/requirements.txt
 else
-    source ${VENV_DIR}/bin/activate
+    source "${VENV_DIR}/bin/activate"
 fi
